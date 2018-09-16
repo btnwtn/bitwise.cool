ece2edb814ee8ac836fdc89dba9ea96174546b8c
Brandon Newton <btnwtn@gmail.com>
2018-09-15 21:26:09 -0400

    add source-fs and remark
    * add remark and source-fs
    * rm unused file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,14 @@ module.exports = {
     title: 'bitwise.cool',
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      {posts.map(({ node: post }) => (
+        <Link to={post.fields.slug} key={post.id}>
+          {post.frontmatter.title}
+        </Link>
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+          excerpt(pruneLength: 80)
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
