436c9c6405fd2f51b464b929b8909877a3592687
Brandon Newton <btnwtn@gmail.com>
2018-09-28 15:00:23 -0400

    getting started with metadata

--- src/templates/content.js
+++ src/templates/content.js
@@ -4,16 +4,35 @@ import Layout from '../components/layout'
 
 export default ({ data }) => {
   const post = data.markdownRemark
-  const files = data.allFile.edges
+  const files = data.allFile.edges.filter(
+    ({ node }) => node.childImageSharp !== null
+  )
+  const metadatum = data.allFile.edges.filter(
+    ({ node }) => node.name !== 'index'
+  )
+
+  debugger
 
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        {files.map(({ node }) => (
-          <img src={node.publicURL} />
-        ))}
+        {files.map(({ node }) => {
+          const filename = `${node.name}${node.ext}`
+
+          return (
+            <div key={filename}>
+              <a href={node.childImageSharp.original.src}>
+                <img
+                  alt={filename}
+                  src={node.childImageSharp.resize.src}
+                  srcSet={node.childImageSharp.sizes.srcSet}
+                />
+              </a>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   )
@@ -27,12 +46,22 @@ export const query = graphql`
         title
       }
     }
-    allFile(
-      filter: { relativePath: { regex: $slug }, extension: { ne: "md" } }
-    ) {
+    allFile(filter: { relativePath: { regex: $slug } }) {
       edges {
         node {
-          publicURL
+          ext
+          name
+          childImageSharp {
+            original {
+              src
+            }
+            resize(width: 920) {
+              src
+            }
+            sizes(maxWidth: 1240) {
+              srcSet
+            }
+          }
         }
       }
     }
